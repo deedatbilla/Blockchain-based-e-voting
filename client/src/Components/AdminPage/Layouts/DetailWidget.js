b546e5903090d8078b6b58ca0d2823bd9fd816fa
deedat5 <deedat5@yahoo.com>
2020-01-04 18:31:08 +0000

    created detailswidget component and newelectionform component

---
+++ client/src/Components/AdminPage/Layouts/DetailWidget.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default function DetailWidget(props) {
+    return (
+        <div>
+            <div className="row">
+              <div className="col-md-3">
+          <div className="box box-success box-solid">
+            <div className="box-header with-border">
+    <h3 className="box-title">{props.prezname}</h3>
+
+              <div className="box-tools pull-right">
+                <button type="button" className="btn btn-box-tool" data-widget="remove"><i className="fa fa-times"></i></button>
+              </div>
+              
+            </div>
+            
+            <div className="box-body">
+              {props.prezparty}
+            </div>
+            
+          </div>
+          
+        </div>
+        </div>
+        </div>
+    )
+}
--- client/src/Components/AdminPage/Layouts/NewElectionForm.js
+++ client/src/Components/AdminPage/Layouts/NewElectionForm.js
@@ -1,9 +1,107 @@
-import React from 'react'
-
+import React from "react";
+import DetailWidget from './DetailWidget'
 export default function NewElectionForm() {
-    return (
-        <div>
+  return (
+    <div>
+      <div className="content-wrapper" style={{ minHeight: "475px" }}>
+        <section className="content">
+<DetailWidget prezname="Elon Musk" prezparty="SpaceX"/>
+          <div className="row">
+           
+          <div className="col-md-6">
+              <div className="box box-primary">
+                <div className="box-header with-border">
+                  <h3 className="box-title">Add a presidential candidate</h3>
+                </div>
+
+                <form role="form">
+                  <div className="box-body">
+                    <div className="form-group">
+                      <label htmlFor="exampleInputEmail1">Candidate Fullname</label>
+                      <input
+                        type="text"
+                        name="prezname"
+                        className="form-control"
+                        id="exampleInputEmail1"
+                        placeholder="Enter candidate name "
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="exampleInputPassword1">Party Name</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="exampleInputPassword1"
+                        name="prezparty"
+                        placeholder="Enter party name"
+                      />
+                    </div>
+
+                    <div className="box-footer">
+                      <button type="submit" className="btn btn-primary">
+                        Submit
+                      </button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+
+           
+            <div className="col-md-6">
+              <div className="box box-primary">
+                <div className="box-header with-border">
+                  <h3 className="box-title">Add a parliamentary candidate</h3>
+                </div>
+
+                <form role="form">
+                  <div className="box-body">
+                    <div className="form-group">
+                      <label htmlFor="exampleInputEmail1">Candidate Fullname</label>
+                      <input
+                        type="text"
+                        name="parlname"
+                        className="form-control"
+                        id="exampleInputEmail1"
+                        placeholder="Enter candidate name "
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="exampleInputPassword1">Party Name</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="exampleInputPassword1"
+                        name="parlparty"
+                        placeholder="Enter party name"
+                      />
+                    </div>
+
+                    <div className="form-group">
+                      <label htmlFor="exampleInputPassword1">District</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="exampleInputPassword1"
+                        name="district"
+                        placeholder="Enter candidate district"
+                      />
+                    </div>
+
+                    <div className="box-footer">
+                      <button type="submit" className="btn btn-primary">
+                        Submit
+                      </button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+
             
-        </div>
-    )
+          </div>
+        </section>
+      </div>
+    </div>
+  );
 }
