94ff58859ff780378b22db47da47ec1e86d765c0
deedat5 <deedat5@yahoo.com>
2020-01-10 10:49:50 +0000

    createed votingpageheader component

---
+++ client/src/Components/Client/Layouts/Votingpageheader.js
@@ -0,0 +1,73 @@
+import React from 'react'
+
+export default function Votingpageheader() {
+    return (
+        <div>
+              <header class="tr-header">
+      <nav class="navbar navbar-default">
+       <div class="container">
+	    <div class="navbar-header">
+		 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
+		  <span class="sr-only">Toggle navigation</span>
+		  <span class="icon-bar"></span>
+		  <span class="icon-bar"></span>
+		  <span class="icon-bar"></span>
+		 </button>
+		 <a class="navbar-brand" href="https://www.themashabrand.com/scripts/Voting/">Voting</a>
+		</div>
+		<div class="navbar-left">
+		 <div class="collapse navbar-collapse" id="navbar-collapse">
+		  <ul class="nav navbar-nav">
+		   <li class="active"><a href="https://www.themashabrand.com/scripts/Voting/">Home</a></li>
+		   <li class=""><a href="https://www.themashabrand.com/scripts/Voting//how">How it works</a></li>
+		  </ul>
+		 </div>
+		</div>
+		<div class="navbar-right">  
+
+		 <ul class="nav navbar-nav">
+              <li class="dropdown user user-menu">
+             
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                
+                 
+                  <span class="hidden-xs">
+                  	Languages                  </span>
+                </a>
+                
+              </li>
+           </ul>  
+		 	
+		 		 	
+		 					 
+			
+		  
+		 <ul class="nav navbar-nav">
+		 <li class="dropdown mega-avatar">
+		  <a href="https://www.themashabrand.com/scripts/Voting/nominees/214407771051#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">
+		   <span class="avatar w-32"><img src="./voting page_files/1515503485.jpg" class="img-resonsive img-circle" width="25" height="25" alt="..."/></span>
+		 
+		   <span>
+			Avatar Twenty		   </span>
+		  </a>
+		  <div class="dropdown-menu w dropdown-menu-scale pull-right">
+		   <a class="dropdown-item" href="https://www.themashabrand.com/scripts/Voting/account/dashboard"><span>Dashboard</span></a> 
+		   <a class="dropdown-item" href="https://www.themashabrand.com/scripts/Voting/account/profile"><span>Profile</span></a>
+		   <a class="dropdown-item" href="https://www.themashabrand.com/scripts/Voting/account/logout">Logout</a>
+		  </div>
+		 </li>
+		 </ul>
+		 
+		 		 
+		 		 		
+		</div>
+       </div>
+      </nav>
+     </header>
+ 
+ 	 
+  
+	
+        </div>
+    )
+}
