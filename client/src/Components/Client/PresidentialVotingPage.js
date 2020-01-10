4ac0cb3751ffacf244311707bb9d29bac983829c
deedat5 <deedat5@yahoo.com>
2020-01-10 10:54:27 +0000

    created component for presidential voting page

---
+++ client/src/Components/Client/PresidentialVotingPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Votingpageheader from './Layouts/Votingpageheader'
+
+ class PresidentialVotingPage extends Component {
+    render() {
+        return (
+            <div>
+                <Votingpageheader/>
+            </div>
+        )
+    }
+}
+export default PresidentialVotingPage
