25bfd683fc7c019ebf8ea36dfc3289351aecd8f0
deedat5 <deedat5@yahoo.com>
2020-01-13 17:52:17 +0000

    created block explorer component

---
+++ client/src/Components/Client/Blockexplorer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+ class Blockexplorer extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+export default Blockexplorer
