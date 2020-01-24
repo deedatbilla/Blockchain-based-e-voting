d38da20ae39001f8a6de9fb5b056cb38bfbacafc
deedat5 <deedat5@yahoo.com>
2020-01-24 01:00:46 +0000

    added wallet creation data to the json request for adding voters

--- client/src/Components/AdminPage/Addvoter.js
+++ client/src/Components/AdminPage/Addvoter.js
@@ -6,32 +6,44 @@ import Sidebar from "./Layouts/Sidebar";
 import AdminHeader from "./Layouts/AdminHeader";
 import Footer from "./Layouts/Footer";
 import axios from "axios";
+import { setconnection } from "../../actions/connectActions";
+import { connect } from "react-redux";
 class Addvoter extends Component {
-    state = {
+  state = {
+    name: "",
+    voter_id: "",
+    email: "",
+    constituency: "",
+    password: ""
+  };
+  render() {
+    onchange = e => {
+      this.setState({ [e.target.name]: e.target.value });
+    };
+
+    onsubmit = async e => {
+      e.preventDefault();
+      const { web3 } = this.props;
+
+      const { privateKey, address } = web3.eth.accounts.create();
+      const cred = this.state;
+      const user_data = {
+        ...cred,
+        privateKey,
+        address
+      };
+      const response = await axios.post(
+        "http://localhost:5000/users",
+        user_data
+      );
+      this.setState({
         name: "",
         voter_id: "",
         email: "",
         constituency: "",
         password: ""
-      };
-  render() {
-   
-    onchange = e => {
-      this.setState({ [e.target.name]: e.target.value });
-    };
-    onsubmit = async e => {
-        e.preventDefault()
-        
-       const response = await axios.post("http://localhost:5000/users", this.state);
-        this.setState({
-            name: "",
-            voter_id: "",
-            email: "",
-            constituency: "",
-            password: ""
-        })
-
-        console.log(this.state)
+      });
+      console.log(response);
     };
     return (
       <div>
@@ -51,4 +63,8 @@ class Addvoter extends Component {
     );
   }
 }
-export default Addvoter;
+
+const mapStateToProps = state => ({
+  web3: state.connect.web3
+});
+export default connect(mapStateToProps, { setconnection })(Addvoter);
